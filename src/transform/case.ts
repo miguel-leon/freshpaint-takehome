8f5379292d136a91c02ec3e182f3e7a3a507da49
Miguel Leon <miguel.e.leon.f@gmail.com>
2022-03-12 16:05:07 -0300

    🎈 case transformation complete.

--- src/transform/case.ts
+++ src/transform/case.ts
@@ -2,6 +2,12 @@ import { Data } from '@src/transform/transform';
 import { Case } from '@src/transformation';
 
 
+const CASE_FUNCTIONS = {
+	lowercase: String.prototype.toLowerCase,
+	uppercase: String.prototype.toUpperCase
+};
+
 export function tr_case(data: Data, { type, property }: Case): Data {
-	return data;
+	const { [property]: value, ...rest } = data;
+	return { ...rest, [property]: CASE_FUNCTIONS[type].call(value) };
 }
--- tests/transform/case.test.ts
+++ tests/transform/case.test.ts
@@ -8,4 +8,10 @@ describe('transformation case', () => {
 		const result = tr_case(data, { type: 'lowercase', property: 'a' });
 		expect(result).toEqual({ a: 'xyz', b: 10 });
 	});
+
+	test('uppercase property value', () => {
+		const data = { a: 'Xyz', b: 10 };
+		const result = tr_case(data, { type: 'uppercase', property: 'a' });
+		expect(result).toEqual({ a: 'XYZ', b: 10 });
+	});
 });
